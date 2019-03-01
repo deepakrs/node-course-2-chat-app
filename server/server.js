788a2b78ef869099c290769a3222e866641b0c71
deepak <shindedeepak159@gmail.com>
2019-02-28 21:25:57 -0800

    Add Users class and updateUSerList event

--- server/server.js
+++ server/server.js
@@ -4,20 +4,43 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat App'));
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required');
+    }
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+    socket.join(params.room);
+    users.removeUser(socket.id); //remove them from prev rooms
+    users.addUser(socket.id, params.name, params.room); //add them to new room
+
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+    // socket.leave('The office fans');
+
+    // io.emit - emits to every single connected user
+      // io.to('The office fans').emit
+    // socket.broadcast.emit - sends message to everyone connected to the socket server except for the current user
+      // socket.broadcast.to('The office fans').emit
+    // socket.emit
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat App'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    callback();
+  });
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
@@ -30,7 +53,13 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('User was disconnected');
+    // console.log('User was disconnected');
+    var user = users.removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room)); // update the user list
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+    }
   });
 });
 
